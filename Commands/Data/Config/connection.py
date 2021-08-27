166a471c012768324584c3f8b14d088b08f229b4
Ivan1343 <75171093+Ivan1343@users.noreply.github.com>
2021-08-27 16:58:59 +0300

    Add files via upload

---
+++ Commands/Data/Config/connection.py
@@ -0,0 +1,3 @@
+token = '264e7b4edb47cdecd7dbc6e4c0509cebdbdbf03e018af1c7da246401f48d8bf9b74669d3e19791a60e353' #Токен должен быть в ковычках!
+access_token = 'd72c1d46592a383bc8607ebf9e1c4a4ff6d049b2e9f92f392d80f8fbc52b7951b2ca96ac53868063a4c01' #Токен должен быть в ковычках!
+group_id = 197481314 #Айди должно быть без ковычек!
