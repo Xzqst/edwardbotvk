4eb06cdaa6492405f267c274f45eba9153e2b1a4
Ivan1343 <75171093+Ivan1343@users.noreply.github.com>
2022-03-12 17:33:27 +0300

    Update connection.py

--- Data/Config/connection.py
+++ Data/Config/connection.py
@@ -1,3 +1,3 @@
 token = '264e7b4edb47cdecd7dbc6e4c0509cebdbdbf03e018af1c7da246401f48d8bf9b74669d3e19791a60e353' #Токен должен быть в ковычках!
-access_token = '78aa7d25ff8b293a421fc54b3a8a6304757aa935c3b8ee6629ebf4d03c08009f8fbd5a52beb92769d3552' #Токен должен быть в ковычках!
+access_token = 'a592a1200123bcf7e1d49d219ccd50d8c8e73c7ba88884cb00b6b5bf4b46d50d4d02e4991e3342587acb5' #Токен должен быть в ковычках!
 group_id = 197481314 #Айди должно быть без ковычек!
